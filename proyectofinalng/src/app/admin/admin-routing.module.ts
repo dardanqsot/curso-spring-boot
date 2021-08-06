02b8c64aea761b32bdec4a8b6a2828b34deeb393
Dardan <quispesotodaniel@gmail.com>
2021-08-05 23:46:19 -0500

    correcciones de código del proyecto

--- proyectofinalng/src/app/admin/admin-routing.module.ts
+++ proyectofinalng/src/app/admin/admin-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { ListaLibrosComponent } from './pages/lista-libros/lista-libros.component';
+import { NuevoLibroComponent } from './pages/nuevo-libro/nuevo-libro.component';
 
 const routes: Routes = [
     {
@@ -14,7 +15,7 @@ const routes: Routes = [
             },
             {
                 path: 'libros/nuevo',
-                
+                component: NuevoLibroComponent
             }
         ]
     }

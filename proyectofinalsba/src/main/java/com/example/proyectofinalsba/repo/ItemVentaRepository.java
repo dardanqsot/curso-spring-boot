41558172c589dc9f9345b7bc7c69ae14ede2890a
Dardan <quispesotodaniel@gmail.com>
2021-08-10 22:35:13 -0500

    Actualización de clases, código del proyecto

--- proyectofinalsba/src/main/java/com/example/proyectofinalsba/repo/ItemVentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.proyectofinalsba.repo;
-
-import com.example.proyectofinalsba.model.ItemVenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemVentaRepository extends JpaRepository<ItemVenta, Integer> {
-}
--- proyectofinalsba/src/main/java/com/example/proyectofinalsba/repo/VentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.proyectofinalsba.repo;
-
-import com.example.proyectofinalsba.model.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VentaRepository extends JpaRepository<Venta, Integer> {
-}
